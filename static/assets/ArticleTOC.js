4fd83d138f952bd85b0a39cb6a25b985ae2a3288
hst1189 <hst1189@hotmail.com>
2024-09-29 15:23:27 +0900

    Update ArticleTOC.js

--- static/assets/ArticleTOC.js
+++ static/assets/ArticleTOC.js
@@ -189,7 +189,7 @@ document.addEventListener("DOMContentLoaded", function() {
     
     tocIcon.classList.add('active');
     
-    tocIcon.textContent = '☰';
+    tocIcon.textContent = '';
     tocIcon.onclick = (e) => {
         e.stopPropagation();
         toggleTOC();
